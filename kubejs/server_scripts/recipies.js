920b9a1564125ce38ad860267887fa0b6e90ef70
Melodi <62381002+Melodi17@users.noreply.github.com>
2024-11-05 08:35:06 +1000

    updated recipies

--- kubejs/server_scripts/recipies.js
+++ kubejs/server_scripts/recipies.js
@@ -4,6 +4,23 @@ ServerEvents.recipes(event => {
       U: 'ae2:spatial_cell_component_128',
       B: 'ae2:wireless_booster',
       E: '#forge:plates/enderium',
-      N: 'minecraft:netherite_ingot'
+      N: 'minecraft:netherite_block'
     }).id('kubejs:aeinfinitybooster/infinity_card')
   })
+
+  // Botany pots fix
+  ServerEvents.recipes(event => {
+  //   event.recipes.botanypots.crop(
+  //     "minecraft:candle", // seed item
+  //     ["oak_leaves"], // categories that this crop can be planted on
+  //     { block: "minecraft:candle" }, // display block
+  //     [
+  //         Item.of ("minecraft:candle") // item
+  //             .withChance(100) // weight of this entry compared to the others
+  //             .withRolls(1, 2) // the times this loot will be chosen (min, max)
+  //         // for example, when chosen this will give 1 to 2 candles
+  //     ],
+  //     10, // growthTicks
+  //     1, // optional, growthModifier - this can be set to 1 in most cases
+  // )
+  });
