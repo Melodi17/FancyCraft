882942cba123a49e3e015f3708b10adc8adc1b6a
Melodi <62381002+Melodi17@users.noreply.github.com>
2024-11-09 13:55:06 +1000

    added mods, jei stuff and quests

--- kubejs/server_scripts/recipies.js
+++ kubejs/server_scripts/recipies.js
@@ -1,26 +1,35 @@
 ServerEvents.recipes(event => {
-    event.remove({ id: 'aeinfinitybooster:infinity_card' })
-    event.shaped('aeinfinitybooster:infinity_card', ['EBE', 'BUB', 'NNN'], {
-      U: 'ae2:spatial_cell_component_128',
-      B: 'ae2:wireless_booster',
-      E: '#forge:plates/enderium',
-      N: 'minecraft:netherite_block'
-    }).id('kubejs:aeinfinitybooster/infinity_card')
-  })
+  event.remove({ id: 'aeinfinitybooster:infinity_card' })
+  event.shaped('aeinfinitybooster:infinity_card', ['EBE', 'BUB', 'NNN'], {
+    U: 'ae2:spatial_cell_component_128',
+    B: 'ae2:wireless_booster',
+    E: '#forge:plates/enderium',
+    N: 'minecraft:netherite_block'
+  }).id('kubejs:aeinfinitybooster/infinity_card')
+})
 
-  // Botany pots fix
-  ServerEvents.recipes(event => {
-  //   event.recipes.botanypots.crop(
-  //     "minecraft:candle", // seed item
-  //     ["oak_leaves"], // categories that this crop can be planted on
-  //     { block: "minecraft:candle" }, // display block
-  //     [
-  //         Item.of ("minecraft:candle") // item
-  //             .withChance(100) // weight of this entry compared to the others
-  //             .withRolls(1, 2) // the times this loot will be chosen (min, max)
-  //         // for example, when chosen this will give 1 to 2 candles
-  //     ],
-  //     10, // growthTicks
-  //     1, // optional, growthModifier - this can be set to 1 in most cases
-  // )
-  });
+ServerEvents.recipes(event => {
+  event.remove({ output: 'blahaj:yarn_spinner' })
+
+  let knitting = color => {
+    event.shapeless(`5x blahaj:${color}_yarn`, [
+      `#minecraft:swords`,
+      `minecraft:${color}_wool`,
+    ])
+    .damageIngredient('#minecraft:swords', 1)
+    .id(`kubejs:blahaj/knitting_yarn_${color}`)
+  }
+
+  let cheap_knitting = color => {
+    event.shapeless(`2x blahaj:${color}_yarn`, [
+      `minecraft:stick`,
+      `minecraft:${color}_wool`,
+    ])
+    .id(`kubejs:blahaj/knitting_cheap_yarn_${color}`)
+  }
+
+  //  white, orange, magenta, light blue, yellow, lime, pink, gray, light gray, cyan, purple, blue, brown, green, red, and black
+  let colors = ['white', 'orange', 'magenta', 'light_blue', 'yellow', 'lime', 'pink', 'gray', 'light_gray', 'cyan', 'purple', 'blue', 'brown', 'green', 'red', 'black']
+  colors.forEach(knitting)
+  colors.forEach(cheap_knitting)
+});
